4fd75a7d95a006b4e4812265e831f16934a4a187
Achintya Rao <achintya@achintyarao.in>
2024-07-29 16:36:41 +0100

    Add code from `Renewables.purs` to `Article.purs`
    Signed-off-by: Achintya Rao <achintya@achintyarao.in>

--- src/Article.purs
+++ src/Article.purs
@@ -1,8 +1,24 @@
 module Article where
 
-import Prelude
+import Prelude hiding (absurd)
 
+import App.Fig (FigSpec, drawFig, loadFig)
+import App.Util (runAffs_)
+import Bind ((↦))
+import Data.Tuple (uncurry)
 import Effect (Effect)
+import Module (File(..))
+import Util ((×))
+
+-- Delete this test case once we can enable/disable outputs via the UI
+
+figSpec :: FigSpec
+figSpec =
+   { datasets: [ "renewables" ↦ "example/linked-outputs/renewables" ]
+   , imports: []
+   , file: File "linked-outputs/line-chart"
+   , inputs: [ "renewables" ]
+   }
 
 main :: Effect Unit
-main = pure unit
+main = runAffs_ (uncurry drawFig) [ ("fig" × _) <$> loadFig figSpec ]
